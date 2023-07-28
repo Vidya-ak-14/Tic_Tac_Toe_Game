c3598c25cfe85edf8d8885f57d9ac2742da47150
Vidya-ak-14 <92156953+Vidya-ak-14@users.noreply.github.com>
2023-07-28 16:28:08 +0530

    Add files via upload

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,15 @@
+package tictactoe;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Tictactoe tictactoe = new Tictactoe();
+		//tic hello=new tic();
+
+	}
+
+}
